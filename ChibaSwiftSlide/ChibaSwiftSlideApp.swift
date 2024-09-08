3d82d526f66b543e8a002c0676e26489d67d1776
yugo.sugiyama <sugiy.u.2203@gmail.com>
2024-09-08 17:05:32 +0900

    Initial Commit

---
+++ ChibaSwiftSlide/ChibaSwiftSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChibaSwiftSlideApp.swift
+//  ChibaSwiftSlide
+//
+//  Created by Yugo Sugiyama on 2024/09/08.
+//
+
+import SwiftUI
+
+@main
+struct ChibaSwiftSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
